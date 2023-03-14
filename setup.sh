10c032d53b14322cda5b7ab07835d9609160e8f9
Ajeet Yadav <ajeetyadav164@gmail.com>
2023-03-15 03:10:15 +0530

    🐛  BugFix

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+BRANCH_NAME='main'
 BRANCH_LIST=`git branch -r | grep -v -- '->'`
 for branch in ${BRANCH_LIST[@]}
 do  
@@ -13,13 +13,7 @@ if [ `git branch --merged | wc -l` == 1 ]; then
     echo "🥳 No merged branch found"
     exit 0
 else
-    for branch in `git branch --merged`
-    do  
-        DEFAULT_BRANCH="origin/"${BRANCH_NAME}
-        if [ $branch != $DEFAULT_BRANCH ]; then
-            git push origin -d $branch
-        fi
-    done
+    git branch --merged | egrep -v "(^\*|$BRANCH_NAME)" | xargs git push origin -d
     echo "🗑️ Delete All Merged Branch!" 
 fi
 
